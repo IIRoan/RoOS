817a3a6fb4b615af47c4a9f3aecdf5c5d705ea26
roan.van.westerop <github.com.q6c0y@lunary.roan.zip>
2025-06-20 11:35:33 +0200

    Add rpm repo for vscode

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,6 +15,10 @@ dnf5 install -y neovim
 dnf5 install -y git
 dnf5 install -y gcc
 dnf5 install -y make
+
+# Add Microsoft repository and install VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
 dnf5 install -y code
 
 # Container and virtualization tools
