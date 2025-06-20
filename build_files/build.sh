2a0c816d1c3a601ff1a0426a409d787eec39502b
roan.van.westerop <github.com.q6c0y@lunary.roan.zip>
2025-06-20 11:31:16 +0200

    Start on customizations

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,35 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
+# Development tools
+dnf5 install -y android-tools
+dnf5 install -y neovim
+dnf5 install -y git
+dnf5 install -y gcc
+dnf5 install -y make
+dnf5 install -y code
+
+# Container and virtualization tools
+dnf5 install -y docker
+dnf5 install -y podman
+dnf5 install -y distrobox
+dnf5 install -y toolbox
+dnf5 install -y virt-manager
+dnf5 install -y qemu-kvm
+dnf5 install -y libvirt
+
+# Terminal and shell tools
+dnf5 install -y zsh
+dnf5 install -y zsh-autosuggestions
+dnf5 install -y ghostty
+dnf5 install -y yakuake
+
+# Security and network tools
+dnf5 install -y nmap
+dnf5 install -y wireshark
+dnf5 install -y hashcat
+
+# Existing packages
 dnf5 install -y tmux 
 dnf5 install -y btop 
 
@@ -20,6 +48,12 @@ dnf5 install -y btop
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+#### Enable System Unit Files
 
 systemctl enable podman.socket
+systemctl enable docker.socket
+systemctl enable libvirtd.service
+
+#### Add RoOS just recipes
+
+echo "import \"/usr/share/roos/just/roos.just\"" >> /usr/share/ublue-os/justfile
