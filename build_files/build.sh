bbc7d9bbd7948f97bc17a6e88c43e5abb4645596
roan.van.westerop <github.com.q6c0y@lunary.roan.zip>
2025-06-20 12:15:19 +0200

    branding added

--- build_files/build.sh
+++ build_files/build.sh
@@ -60,3 +60,88 @@ systemctl enable libvirtd.service
 #### Add RoOS just recipes
 
 echo "import \"/usr/share/roos/just/roos.just\"" >> /usr/share/ublue-os/justfile
+
+#### Setup RoOS branding
+
+# Create GSettings override for GNOME branding
+mkdir -p /usr/share/glib-2.0/schemas
+cat > /usr/share/glib-2.0/schemas/99-roos-branding.gschema.override << 'EOF'
+[org.gnome.desktop.interface]
+icon-theme='RoOS'
+
+[org.gnome.shell]
+app-picker-view=1
+
+[org.gnome.desktop.background]
+picture-uri='file:///usr/share/pixmaps/roos-logo.png'
+picture-uri-dark='file:///usr/share/pixmaps/roos-logo.png'
+EOF
+
+# Create custom icon theme that includes RoOS branding
+mkdir -p /usr/share/icons/RoOS/scalable/apps
+mkdir -p /usr/share/icons/RoOS/256x256/apps
+mkdir -p /usr/share/icons/RoOS/128x128/apps
+mkdir -p /usr/share/icons/RoOS/48x48/apps
+mkdir -p /usr/share/icons/RoOS/32x32/apps
+mkdir -p /usr/share/icons/RoOS/16x16/apps
+
+# Create icon theme index
+cat > /usr/share/icons/RoOS/index.theme << 'EOF'
+[Icon Theme]
+Name=RoOS
+Comment=RoOS Icon Theme
+Inherits=Adwaita
+Directories=16x16/apps,32x32/apps,48x48/apps,128x128/apps,256x256/apps,scalable/apps
+
+[16x16/apps]
+Size=16
+Context=Applications
+Type=Fixed
+
+[32x32/apps]
+Size=32
+Context=Applications
+Type=Fixed
+
+[48x48/apps]
+Size=48
+Context=Applications
+Type=Fixed
+
+[128x128/apps]
+Size=128
+Context=Applications
+Type=Fixed
+
+[256x256/apps]
+Size=256
+Context=Applications
+Type=Fixed
+
+[scalable/apps]
+Size=256
+Context=Applications
+Type=Scalable
+MinSize=16
+MaxSize=512
+EOF
+
+# Copy RoOS logos to custom icon theme
+cp /usr/share/icons/hicolor/16x16/apps/roos-logo.png /usr/share/icons/RoOS/16x16/apps/
+cp /usr/share/icons/hicolor/32x32/apps/roos-logo.png /usr/share/icons/RoOS/32x32/apps/
+cp /usr/share/icons/hicolor/48x48/apps/roos-logo.png /usr/share/icons/RoOS/48x48/apps/
+cp /usr/share/icons/hicolor/128x128/apps/roos-logo.png /usr/share/icons/RoOS/128x128/apps/
+cp /usr/share/icons/hicolor/256x256/apps/roos-logo.png /usr/share/icons/RoOS/256x256/apps/
+cp /usr/share/icons/hicolor/scalable/apps/roos-logo.svg /usr/share/icons/RoOS/scalable/apps/
+
+# Also replace common system logo references
+cp /usr/share/pixmaps/roos-logo.svg /usr/share/pixmaps/gnome-logo-text.svg
+cp /usr/share/pixmaps/roos-logo.svg /usr/share/pixmaps/gnome-foot.svg
+cp /usr/share/pixmaps/roos-logo.png /usr/share/pixmaps/system-logo-white.png
+
+# Compile GSettings schemas
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Update icon caches
+gtk-update-icon-cache /usr/share/icons/hicolor/ || true
+gtk-update-icon-cache /usr/share/icons/RoOS/ || true
